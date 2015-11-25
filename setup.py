8b08e050ca7a0c1caa9c9ef75c8fb0f9f4aa825d
Caio Belfort <caiobelfort90@gmail.com>
2015-11-25 02:33:53 -0300

    Preparando repositorio para criação de pacote pypi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ArtificialCrawlers',
+      version='0.1',
+      description='An artificial crawlers implementation',
+      url='https://github.com/caiobelfort/ArtificialCrawlers',
+      author='Caio Belfort',
+      author_email='caiobelfort90@gmail.com',
+      license='MIT',
+      packages=['ArtificialCrawlers'],
+      zip_safe=False)
